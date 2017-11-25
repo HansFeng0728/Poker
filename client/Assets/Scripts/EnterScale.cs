7e578edbc807851b3964ef1561858e41bf79f213
xgerool950@gmail.com <xgerool950@gmail.com>
2017-11-25 18:39:46 +0800

    client移动

---
+++ client/Assets/Scripts/EnterScale.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterScale : MonoBehaviour
+{
+	void OnPress(bool isPressed)
+    {
+        if (isPressed)
+            gameObject.transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
+        else
+            gameObject.transform.localScale = Vector3.one;
+    }
+
+}
---
+++ client/Assets/Scripts/SignIn.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SignIn : MonoBehaviour {
+
+    public GameObject lobby;
+    public void Sign()
+    {
+        Instantiate(lobby);
+    }
+}
